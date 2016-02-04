4e4aef99fe704315a4d794ed8316959fef8c117d
oubai <oubai1987@gmail.com>
2016-02-04 22:37:57 +0800

    指针01-改变值

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+void change(int *x);
+
 int main()
 {
-	printf("hello world!\n");
+	int x = 10;
+	change(&x);
+	printf("x=%d\n", x);
 	return 0;
+}
+
+
+void change(int *x)
+{
+	*x = 100;
 }
